b25f8fb85c9c63524b8a76f6db4e4cfe7cabba0c
Trafalgar <guoyawen98@gmail.com>
2025-03-10 10:01:34 +0800

    修正变量类型，gcc 报错，原因是变量类型不匹配, msvc 未报错

--- logviewer/src/util/utils_timer.hpp
+++ logviewer/src/util/utils_timer.hpp
@@ -6,7 +6,7 @@
 
 struct Timer
 {
-    std::chrono::time_point<std::chrono::steady_clock> start, end;
+    std::chrono::time_point<std::chrono::high_resolution_clock> start, end;
     std::chrono::duration<float> duration;
 
     Timer()
